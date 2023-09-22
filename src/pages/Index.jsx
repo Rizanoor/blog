87aeb6635bf310eb3fd617a29e3b793de23d9109
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-22 16:42:54 +0700

    feat: learn useState

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,24 @@
 import posts from "../post.json";
 import Artikel from "../components/atrikel";
+import { useState } from "react";
 
 export default function Homepage() {
+
+  const [search, setSearch] = useState("");
+
+  const changeSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
   return (
     <>
         <h1>simple blog</h1>
-        {posts.map((blog, index) => (
-          <Artikel key={index} title={blog.title} tags={blog.tags} date={blog.date}/>
+        <div>
+          Cari artikel: <input onChange={changeSearch}></input>
+        </div>
+        <div className="">Ditemukan 0 data dengan pencarian kata <b>{search}</b></div>
+        {posts.map(({title, tags, date}, index) => (
+          <Artikel { ...{title, tags, date}} key={index}/>
         ))}
     </>
   )
