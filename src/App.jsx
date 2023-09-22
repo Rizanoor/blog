29f0c286ef62a7e356876e6d9e54a91ff49e88cf
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-22 22:03:12 +0700

    feat: conditioanl renderind component di dalam component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import './App.css'
 import Homepage from './pages';
 
 function App() {
-  const value = false;
   return (
     <>
-      {value ? 'benar' : 'salah'}
       <Homepage/>
     </>
   )
--- src/components/Atrikel.jsx
+++ src/components/Atrikel.jsx
@@ -1,8 +1,22 @@
+const ArtikelStatus = ({ isNew }) => {
+  return isNew && <span> --Baru !!</span>
+}
+
+const NewArtikel = () => {
+  return <span><b> --Baru !!</b></span>
+}
+
 export default function Artikel(props) {
   return (
     <>
       <h3>{props.title}</h3>
-      <small>Date: {props.date}, tags: {props.tags ? props.tags.join(", ") : ""}</small>
+      <small>
+        Date: {props.date}, 
+        tags: {props.tags ? props.tags.join(", ") : ""}
+        {/* {props.isNew && " --Baru !!"} */}
+        {/* <ArtikelStatus isNew={props.isNew} /> */}
+        {props.isNew && <NewArtikel/>}
+      </small>
     </>
   )
 }
