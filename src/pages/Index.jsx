e24bf79c6be84ec3abf87298f31340e0eb949d38
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-24 10:09:38 +0700

    feat: fetch APi with useEffect

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import postsData from "../post.json";
 import Artikel from "../components/atrikel";
 import Search from "../components/Search";
@@ -6,6 +6,7 @@ import Search from "../components/Search";
 export default function Homepage() {
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [externalPosts, setExternalPosts] = useState([]);
 
   const searchChange = (value) => {
       const filteredPosts = postsData.filter((item) => 
@@ -14,12 +15,24 @@ export default function Homepage() {
       setPosts(filteredPosts);
       setTotalPosts(filteredPosts.length);
   }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setExternalPosts(json))
+  }, [])
+
   return (
     <>
         <h1>simple blog</h1>
         <Search onSearchChanges={searchChange} totalPosts={totalPosts}/>
         {posts.map((props, index) => (
           <Artikel { ...props} key={index}/>
+        ))}
+        <hr/>
+        <h2>External Posts</h2>
+        {externalPosts.map((item, index) => (
+          <div key={index}>- {item.title}</div>
         ))}
     </>
   )
