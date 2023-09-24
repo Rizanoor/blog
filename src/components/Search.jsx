331fbaa3512334d8d946bb9f217399f00ba1544b
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-24 09:36:47 +0700

    feat: onClick method on search COmopnent

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,15 +3,26 @@ import { useState } from "react";
 export default function Search(props) {
     
   const [search, setSearch] = useState("");
-  const onSearchChange = (event) => {
-    setSearch(event.target.value)
-    props.onSearchChanges(event.target.value);
+
+  const onSearchChange = () => {
+    props.onSearchChanges(search);
+  }
+
+  const searchKeyDown = (e) => {
+    if ((e.key === "Enter")) {
+      onSearchChange()
+    }
   }
 
   return (
     <>
         <div>
-          Cari artikel: <input onChange={onSearchChange}></input>
+          Cari artikel: 
+          <input 
+            onChange={(e) => setSearch(e.target.value)} 
+            onKeyDown={searchKeyDown}>
+          </input>
+          <button onClick={onSearchChange}>cari</button>
         </div>
         <div className="">Ditemukan {props.totalPosts} data dengan pencarian kata <b>{search}</b></div>
         
