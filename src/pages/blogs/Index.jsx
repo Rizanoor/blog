88d22788efdad6421e9264be6e6a7145759e742b
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-24 12:41:04 +0700

    feat: dynamic parameter ID router

--- src/pages/blogs/Index.jsx
+++ src/pages/blogs/Index.jsx
@@ -1,20 +1,25 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 export default function Blog() {
-    const [externalPosts, setExternalPosts] = useState([]);
+    const [post, setPosts] = useState([]);
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
             .then((response) => response.json())
-            .then((json) => setExternalPosts(json))
+            .then((json) => setPosts(json))
     }, []);
 
     return (
         <>
             <h2>My Blog Posts</h2>
-            {externalPosts.map((item, index) => (
-                <div key={index}>- {item.title}</div>
+            {post.map((item, index) => (
+                <div key={index}>
+                    <Link to={`/blog/${item.id}`}>
+                        - {item.title}
+                    </Link>
+                </div>
             ))}
         </>
     )
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -3,6 +3,7 @@ import RootLayout from "../layouts/RootLayout";
 
 import Homepage from "../pages";
 import About from "../pages/About";
+import Post from "../pages/blogs/_id";
 import Blog from "../pages/blogs";
 
 export const router = createBrowserRouter([
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
             {
                 path: "/blog",
                 element: <Blog/>
+            },
+            {
+                path: "/blog/:id",
+                element: <Post/>
             }
         ]
     },
